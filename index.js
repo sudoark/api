25d02ca876f4fc06ce728293b5e2fed67a1c861a
ARKINUX <88921403+sudoark@users.noreply.github.com>
2024-12-17 18:14:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -90,11 +90,7 @@ app.post('/generate-pdf', (req, res) => {
                 alignment: 'center',
                 margin: [0, 10, 0, 20]
             },
-            { image: './logo.jpg', alignment: 'right', width: 50, margin: [0, 50, 0, 0] },
-            {
-                text: 'Signature',
-                style: 'signature',
-            }
+            { image: './signature.jpg', alignment: 'right', width: 50, margin: [0, 50, 0, 0] }
         ],
         styles: {
             orgName: {
